0aec2c4cbaa5c0f2eaf07cea8fc25c3db9d53784
lkrikl <prosto2590@gmail.com>
2024-02-22 14:40:04 +0300

    add title-block

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.app')
+@section('title-block')
+	Главная страница
+@endsection	
 @section('content')
 	<h1>Главная страница</h1>
 @endsection	
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.app')
+@section('title-block')
+	Страница новостей
+@endsection
 @section('content')
 	<h1>Страница новостей</h1>
 @endsection	
