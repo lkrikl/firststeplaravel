74efc18e742f439a2736fc47fd394bd8b6de9df3
lkrikl <prosto2590@gmail.com>
2024-02-28 15:53:14 +0300

    Add css and header

---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        <span class="fs-4">Kontur2706.ru</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link" aria-current="page">Сервисы</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Контакты</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Техподдержка</a></li>
+        <button class="btn btn-primary rounded-pill px-3" type="button">Обратный звонок</button>
+      </ul>
+    </header>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,15 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>@yield('title-block')</title>
+	@vite(['resources/css/app.css'])
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 <body>
+<div class="container">
+  	@include('inc.header')
 	@yield('content')
 
 	@include('inc.aside')
+</div>
 </body>
 </html>
